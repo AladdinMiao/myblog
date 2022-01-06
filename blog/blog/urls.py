8d653026e0e409f66ba4e31f18a10600187d13eb
xvliang <xvliang@qq.com>
2022-01-06 20:42:22 +0800

    项目准备完成

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import logging
+from django.http import HttpResponse
+
+logger = logging.getLogger('django')
+
+def log(request):
+    logger.info('info')
+    return HttpResponse('test')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', log),
 ]
